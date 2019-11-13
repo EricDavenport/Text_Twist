7eb99a77bca93114707178641be51a697f0e2397
Jaheed Haynes <jaheedhaynes@pursuit.org>
2019-11-13 16:07:26 -0500

    finished project. Altered original project into a custom theme for add value to user experience

--- TextTwistProject/TextTwistProject/ViewController.swift
+++ TextTwistProject/TextTwistProject/ViewController.swift
@@ -10,11 +10,100 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var scrambledLabel: UILabel!
+    
+    @IBOutlet weak var inputField: UITextField!
+    @IBOutlet weak var rightWrongLabel: UILabel!
+    
+    var newWord = Word.getRandomWord()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        inputField.delegate = self
+        
+        scrambledLabel.text = newWord.scrambled
+        print(newWord)
     }
 
+    func checkAnswer() {
+        let userInput = inputField.text?.lowercased() ?? ""
+        
+        if userInput == newWord.unscrambled {
+      
+            rightWrongLabel.text = "CORRECT"
+            rightWrongLabel.backgroundColor = .green
+            newScrambledWord()
+        } else {
+           
+            rightWrongLabel.text = "WRONG!, Try Again"
+            rightWrongLabel.backgroundColor = .red
+        }
+        
+        
+    }
+    
+    func newScrambledWord() {
+        newWord = Word.getRandomWord()
+        scrambledLabel.text = newWord.scrambled
+        rightWrongLabel.backgroundColor = .clear
+    }
+    
+    
+    
+    }
+    
+extension ViewController: UITextFieldDelegate {
 
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        
+//        if textField == inputField {
+//
+//        if let oldString = textField.text {
+//            let newString = oldString.replacingCharacters(in: Range(range, in: oldString)!, with: string)
+//
+//
+//
+//            print(oldString)
+//            print(newString)
+//            print(string)
+//
+//        }
+//
+//    }
+        if scrambledLabel.text?.contains(string) ?? false{
+            //searching through the scramble word for the first location
+                scrambledLabel.text?.remove(at: (scrambledLabel.text?.firstIndex(of: Character(string)))!)
+            } else if string == ""{
+               
+                scrambledLabel.text?.insert(textField.text?.last ?? "z", at: scrambledLabel.text!.endIndex)
+            } else {
+                return false
+            }
+    return true
+    }
+    
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        // dismiss key board
+        textField.resignFirstResponder()
+        
+        checkAnswer()
+        
+        // clears the text field
+        textField.text = nil
+        
+        //newScrambledWord()
+        return true
+    }
+    
+  
 }
 
